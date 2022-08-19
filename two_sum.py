1aac0e5a4510f089f7e558d327f5004fe03b72ea
Bolaji Disu <abolajiidiisu@gmail.com>
2022-08-19 16:31:09 +0100

    first commit

---
+++ two_sum.py
@@ -0,0 +1,21 @@
+# Method one
+def two_sum(arr: list, ans: int) -> list:
+    result = [-1, -1]
+
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            if arr[i] + arr[j] == ans:
+                result[0] = i
+                result[1] = j
+
+    return result
+
+
+# Method two
+
+def two_sum2(arr: list, ans: int) -> list:
+    for i in range(len(arr) - 1):
+        for j in range(i + 1, len(arr)):
+            if arr[i] + arr[j] == ans:
+                return [i, j]
+    return [-1, -1]
